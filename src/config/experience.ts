d5ee2d9aebc6ff056bce3f0b2ebb3a280a8e2758
muruganantham <muruganantham2399@gmail.com>
2025-06-14 00:27:52 +0530

    about-commit

--- src/config/experience.ts
+++ src/config/experience.ts
@@ -15,21 +15,21 @@ export const JOB_EXPERIENCE = [
     company: 'InnogenX',
     location: 'Bengaluru, Karnataka',
     url: 'https://innogenx.in/',
-    startDate: '2024-08-01',
-    endDate: '2025-03-31',
+    startDate: '2024-02-01',
+    endDate: '2025-04-30',
     description:
       'Led the development of a role-based e-commerce platform for admins, distributors, and customers. Improved UI/UX by 60% and optimized deployment using Hostinger VPS. Tech stack: JavaScript, ReactJS, NodeJS, ExpressJS, MySQL, Redux.',
   },
-  {
-    ocupation: 'Freelance Software Developer',
-    company: 'Self-employed / Freelance',
-    location: 'Remote',
-    url: '#',
-    startDate: '2024-02-01',
-    endDate: '2024-07-31',
-    description:
-      'Developed the front end of a React and Next.js-based educational platform for a UAE client, integrating multilingual support via Google Translate API and implementing server-side rendering (SSR) to improve SEO and performance. Tech stack: ReactJS, Next.js, TypeScript, Redux, HTML5, CSS3.',
-  },
+  // {
+  //   ocupation: 'Freelance Software Developer',
+  //   company: 'Self-employed / Freelance',
+  //   location: 'Remote',
+  //   url: '#',
+  //   startDate: '2024-02-01',
+  //   endDate: '2024-07-31',
+  //   description:
+  //     'Developed the front end of a React and Next.js-based educational platform for a UAE client, integrating multilingual support via Google Translate API and implementing server-side rendering (SSR) to improve SEO and performance. Tech stack: ReactJS, Next.js, TypeScript, Redux, HTML5, CSS3.',
+  // },
   {
     ocupation: 'Software Engineer',
     company: 'Aaludra Technology',
